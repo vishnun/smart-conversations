f1e03d1777addc0c5e97bb6d93fba5da4030e0a1
Vishnu <vishnunarang2000@gmail.com>
2016-11-28 17:08:34 -0700

    Adding language select and topic select.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,4 +1,38 @@
 $(function() {
+
+    function getLang() {
+        return $('.lang-select option:selected').text();
+    }
+
+    function initSelect() {
+        $('select').material_select();
+    }
+
+    function setupLanguages(langs) {
+        $('.lang-select').html('');
+        langs.forEach(function(lang, index) {
+            $('.lang-select').append('<option value="' + lang + '">' + lang + '</option>');
+        });
+    }
+
+    function setupTopics() {
+        var topics = window.lcl.topics,
+            topicSelect = $('.topic-select');
+        for (topic in topics) {
+            topicSelect.append('<option value="' + topic + '">' + topic + '</option>');
+        }
+    }
+
+    function getInterestWords() {
+        var selectedTopic = $('.topic-select option:selected').val();
+        return window.lcl.topics[selectedTopic];
+    }
+
+    function getGrammar() {
+        var interestWords = getInterestWords();
+        return '#JSGF V1.0; grammar interest_words; public <interest_words> = ' + interestWords.join(' | ') + ' ;';
+    }
+
     function init() {
         var started = false,
             reset = false;
@@ -7,32 +41,47 @@ $(function() {
         var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
         var criticalWord = $('.critical-word');
         var wordContainer = $('.word-container');
-        var interest_words = ['hello', 'hi', 'morning', 'afternoon', 'evening'];
 
-        var grammar = '#JSGF V1.0; grammar interest_words; public <interest_words> = ' + interest_words.join(' | ') + ' ;';
+        var languages_supported = ['en-US', 'en-GB', 'en-IN', 'en-CA', 'en-AU', 'en-NZ', 'en-ZA'];
+
 
         var recognition = new SpeechRecognition();
         var speechRecognitionList = new SpeechGrammarList();
 
         recognition.continuous = true;
-        speechRecognitionList.addFromString(grammar, 1);
-
-        recognition.grammars = speechRecognitionList;
-        //recognition.continuous = false;
-        recognition.lang = 'en-US';
         recognition.interimResults = true;
         recognition.maxAlternatives = 2;
 
+        recognition.onresult = function(event) {
+            var last = event.results.length - 1;
+            var lastSentence = event.results[last][0].transcript;
+            getInterestWords().forEach(function(element, index) {
+                if (lastSentence.indexOf(element) != -1) {
+                    if (criticalWord.text() == '' && reset == false) {
+                        criticalWord.text(element);
+                    }
+                }
+            });
+            if (event.results[last].isFinal) {
+                reset = false;
+            }
+        }
+
         $(window).keyup(function(evt) {
             evt = evt || window.event;
-            // If Space
-            if (evt.keyCode == '32') {
+            var space = '32';
+            if (evt.keyCode == space) {
                 criticalWord.text('');
                 reset = true;
             }
         });
 
         $('#start-btn').on('click', function(e) {
+            recognition.lang = getLang();
+            var grammar = getGrammar();
+            speechRecognitionList.addFromString(grammar, 1);
+            recognition.grammars = speechRecognitionList;
+
             if (started) {
                 started = false;
                 recognition.stop();
@@ -52,21 +101,10 @@ $(function() {
             }
         });
 
-        recognition.onresult = function(event) {
-            var last = event.results.length - 1;
-            var lastSentence = event.results[last][0].transcript;
-            interest_words.forEach(function(element, index) {
-                if (lastSentence.indexOf(element) != -1) {
-                    if (criticalWord.text() == '' && reset == false) {
-                        criticalWord.text(element);
-                    }
-                }
-            });
-            if (event.results[last].isFinal) {
-                reset = false;
-            }
-        }
+        setupTopics();
+        setupLanguages(languages_supported);
+        initSelect();
     }
-    init();
 
+    init();
 });
---
+++ javascripts/data.js
@@ -0,0 +1,5 @@
+window.lcl = window.lcl ? window.lcl : {};
+window.lcl.topics = {
+	greetings: ['hello', 'hi', 'morning', 'afternoon', 'evening'],
+	sports: ['hockey', 'football', 'cricket', 'soccer']
+}
