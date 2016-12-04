e5c03d8d4d940db62bd7bbabb2ca5f31f634c19c
Vishnu N <vishnunarang2000@gmail.com>
2016-12-04 10:13:13 -0700

    Updating gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='smart-conversations',
+    version='1',
+    packages=[''],
+    url='localhost:8000',
+    license='MIT',
+    author='vishnunarang',
+    author_email='vishnunarang2000@gmail.com',
+    description='Application using web speech api for enabling smarter conversations.'
+)
