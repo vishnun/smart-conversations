cd3333e8d253e7f4397a2099c6d325155b8bb204
Vishnu <vishnunarang2000@gmail.com>
2017-02-06 12:02:29 -0700

    adding more words for diffusion

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,7 +1,7 @@
 window.lcl = window.lcl ? window.lcl : {};
 window.lcl.topics = {
-    diffusion: ['continuous', 'unpredictable', 'random', 'flow', 'equilibrium', 'predictable', 'visible', 'independent',
-        'backwards', 'concentration', 'ratio', 'percent', 'dynamic', 'inanimate', 'intend', 'intention', 'collective', 'uniform', 'simultaneous', 'visible', 'invisible'],
+    diffusion: ['continuous', 'unpredictable', 'random', 'flow', 'equilibrium', 'predictable', 'predict', 'predicted', 'visible', 'independent',
+        'backwards', 'concentration', 'concentrated', 'concentrate', 'ratio', 'percent', 'dynamic', 'inanimate', 'intend', 'intention', 'collective', 'uniform', 'simultaneous', 'visible', 'invisible'],
     greetings: ['hello', 'hi', 'morning', 'afternoon', 'evening'],
     sports: ['hockey', 'football', 'cricket', 'soccer'],
     music: ['rock', 'rap', 'country'],
